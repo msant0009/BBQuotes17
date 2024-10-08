c590fe483ffca3789feaf67a922fd12f3e75765e
Mark Santoro <mark_santoro@comcast.net>
2024-10-07 19:00:53 -0700

    Initial Commit

---
+++ BBQuotes17/BBQuotes17App.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotes17App.swift
+//  BBQuotes17
+//
+//  Created by Mark Santoro on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotes17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
