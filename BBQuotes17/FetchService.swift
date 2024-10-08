712e4675967c0dab3a7c7843da46f000b77c399c
Mark Santoro <mark_santoro@comcast.net>
2024-10-08 12:04:55 -0700

    complete all url data fetching

--- BBQuotes17/FetchService.swift
+++ BBQuotes17/FetchService.swift
@@ -15,6 +15,7 @@ struct FetchService {
     let baseURL = URL(string: "https://breaking-bad-api-six.vercel.app/api")!
     
     //https://breaking-bad-api-six.vercel.app/api/quotes/random?production=Breaking+Bad
+    
     func fetchQuote(from show: String) async throws -> Quote{
         // Build fetch url
         let quoteURL = baseURL.appending(path: "quotes/random")
@@ -22,12 +23,64 @@ struct FetchService {
         
         
         // fetch data
+        let (data, response) = try await URLSession.shared.data(from: fetchURL) // returns a tuple (2 returns in one call)
         
         // handle response
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200
+        else {
+            throw FetchError.badResponse
+        }
         
         // decode data
+        let quote = try JSONDecoder().decode(Quote.self, from: data)
         
         // return quote
+        return quote
+    }
+    
+    func fetchCharacter(_ name: String) async throws -> Character {
+        let characterURL = baseURL.appending(path: "characters")
+        let fetchURL = characterURL.appending(queryItems: [URLQueryItem(name:"name", value: name)])
+        
+        
+        let (data, response) = try await URLSession.shared.data(from: fetchURL) // returns a tuple (2 returns in one call)
+    
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 // 200 = ok
+        else {
+            throw FetchError.badResponse
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let characters = try decoder.decode([Character].self, from: data)
+        
+        return characters[0]
+    }
+    
+    func fetchDeath(for character: String) async throws -> Death? {
+        let fetchURL = baseURL.appending(path: "deaths")
+        
+        let (data, response) = try await URLSession.shared.data(from: fetchURL) // returns a tuple (2 returns in one call)
+    
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 // 200 = ok
+        else {
+            throw FetchError.badResponse
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let deaths = try decoder.decode([Death].self, from: data)
+        
+        for death in deaths {
+            if death.character == character {
+                return death
+            }
+            
+        }
+        
+        return nil
         
     }
+        
 }
