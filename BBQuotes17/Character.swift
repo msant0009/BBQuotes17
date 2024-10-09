9ad57ecd74dd475ce7aaf592b2e175d40700717f
Mark Santoro <mark_santoro@comcast.net>
2024-10-09 10:33:22 -0700

    decode sample data, init from decoder, fetch real data

--- BBQuotes17/Character.swift
+++ BBQuotes17/Character.swift
@@ -17,4 +17,36 @@ struct Character: Decodable {
     let portayedBy: String
     var death: Death?
     
+    enum CodingKeys: CodingKey {
+        case name
+        case birthday
+        case occupations
+        case images
+        case aliases
+        case status
+        case portayedBy
+        // death case added below in init
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        birthday = try container.decode(String.self, forKey: .birthday)
+        occupations = try container.decode([String].self, forKey: .occupations)
+        images = try container.decode([URL].self, forKey: .images)
+        aliases = try container.decode([String].self, forKey: .aliases)
+        status = try container.decode(String.self, forKey: .status)
+        portayedBy = try container.decode(String.self, forKey: .portayedBy)
+        
+        let deathDecoder = JSONDecoder()
+        deathDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+
+        let deathData = try Data(contentsOf: Bundle.main.url(forResource: "sampledeath", withExtension: "json")!)
+        death = try deathDecoder.decode(Death.self, from: deathData)
+    
+    }
+    
+
+    
 }
